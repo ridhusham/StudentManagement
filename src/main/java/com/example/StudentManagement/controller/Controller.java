f7aeaa58932d714c3045e01e6110324c1704a89e
Ridhusha <ridhushaji@gmail.com>
2021-09-01 02:37:13 +0530

    added other details

--- src/main/java/com/example/StudentManagement/controller/Controller.java
+++ src/main/java/com/example/StudentManagement/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.example.StudentManagement.controller;
 import com.example.StudentManagement.database.MemoryDao;
 import com.example.StudentManagement.domain.Student;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,26 +15,18 @@ public class Controller {
         return "Greetings from Spring Boot!";
     }
 
-    @RequestMapping("/test")
-    public String indexTest() {
-        return "Hello test";
-    }
 
-    @RequestMapping("/newfunc")
-    public String newfunc() {
-        return "Hello New Function";
-    }
-
-
-    @RequestMapping("/listStudenxt")
+    @RequestMapping("/listStudent")
     public List<Student> listAllStudent() {
         return this.memoryDao.readAll();
     }
 
-    @GetMapping("/addStudent/{name}")
-    public boolean addStudent(@PathVariable String name){
+    @GetMapping("/addStudent/{name,phoneNumber,email,address}")
+    public boolean addStudent(@PathVariable String name,int phoneNumber,String email){
         Student student=new Student();
         student.setName(name);
+        student.setPhoneNumber(phoneNumber);
+        student.setName(email);
         System.out.println(student);
         return this.memoryDao.save(student);
     }
@@ -42,8 +35,6 @@ public class Controller {
     public Student findStudent(@PathVariable String name){
        return this.memoryDao.findOne(name);
     }
-
-
 
 
 }
--- src/main/java/com/example/StudentManagement/domain/Student.java
+++ src/main/java/com/example/StudentManagement/domain/Student.java
@@ -1,16 +1,32 @@
 package com.example.StudentManagement.domain;
 
+
 public class Student {
 
     private String name;
+    private Integer phoneNumber;
+    private String email;
     private Address address;
 
     @Override
     public String toString() {
         return "Student{" +
                 "name='" + name + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                ", address=" + address +
                 '}';
     }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
 
     public String getName() {
         return name;
